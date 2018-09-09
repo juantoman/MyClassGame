7f581f6d40dab37c120769496b46099b55e8f50a
juantoman <juan.torres@iestacio.com>
2018-09-09 22:47:10 +0200

    Correu recuperació password

--- server/main.js
+++ server/main.js
@@ -1,5 +1,36 @@
 import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
+
+
 
 Meteor.startup(() => {
-  // code to run on server at startup
+  process.env.MAIL_URL = "smtps://myclassgame%40gmail.com:mcgholita06@smtp.gmail.com:465";
 });
+
+Accounts.emailTemplates.siteName = '@MyClassGame';
+
+Accounts.emailTemplates.from = '@MyClassGame Admin <myclassgame@gmail.com>';
+
+Accounts.emailTemplates.enrollAccount.subject = (user) => {
+  return `Welcome to @MyClassGame, ${user.profile.name}`;
+};
+
+Accounts.emailTemplates.enrollAccount.text = (user, url) => {
+  return 'You have been selected to participate in building a better future!'
+    + ' To activate your account, simply click the link below:\n\n'
+    + url;
+};
+
+Accounts.emailTemplates.resetPassword.from = () => {
+  // Overrides the value set in `Accounts.emailTemplates.from` when resetting
+  // passwords.
+  return '@MyClassGame Admin <myclassgame@gmail.com>';
+};
+Accounts.emailTemplates.verifyEmail = {
+   subject() {
+      return "Activate your account now!";
+   },
+   text(user, url) {
+      return `Hey ${user}! Verify your e-mail by following this link: ${url}`;
+   }
+};
