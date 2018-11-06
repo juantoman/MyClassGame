cbade8517e6d542fa127eaff3fe8f9c75a9549c5
Juan <juan.torres@iestacio.com>
2018-11-06 08:19:56 +0000

    Canviar posició botó crear classe

--- client/templates/classes/classes_modals.js
+++ client/templates/classes/classes_modals.js
@@ -19,6 +19,7 @@ Template.classesModals.events({
         teacherId: user._id,
         className: $(event.target).find('[name=class-name]').val(),
         iniHP: 10,
+        stored: false,
         evaluation: 1,
         createdOn: new Date()
       };
