5f3b8c6ae6cbede98ec80737c134aa0e4ee4fe3b
Juan <juan.torres@iestacio.com>
2019-01-28 21:38:24 +0000

    chart.js

--- client/templates/student/studentPage.js
+++ client/templates/student/studentPage.js
@@ -1,6 +1,49 @@
+import Chart from 'chart.js';
+
+function grafica() {
+  var ctx = document.getElementById("myChart");
+  var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+  });
+}
+
 Template.studentPage.onRendered(function() {
    $.getScript("https://widget.cloudinary.com/v2.0/global/all.js");
    $.getScript("https://media-library.cloudinary.com/global/all.js");
+   grafica();
 });
 
 Template.studentPage.helpers({
