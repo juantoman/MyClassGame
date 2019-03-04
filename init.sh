94a5a59b89456a87010a340e18383691a3137218
Juan <juan.torres@iestacio.com>
2019-03-04 08:31:47 +0000

    prova exclude

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #export MONGO_URL=mongodb://adminmcg:mcgholita06@ds113445.mlab.com:13445/myclassgame
 export ROOT_URL=http://myclassgame.iestacio.com
-export API_SendGrid=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
+#export API_SendGrid=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
 #export MAIL_URL="smtps://myclassgame@gmail.com:Mcgholita06?@smtp.gmail.com:465"
 nohup meteor --production --port 3000 &
