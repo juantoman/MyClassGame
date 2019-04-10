0d7f4fbcc9eaa8842f32c6e4a51b3cdbddf42121
Juan <juan.torres@iestacio.com>
2019-04-10 09:13:33 +0000

    Seleccionar estudiant o grup desde random

--- client/templates/tools/randomPage.js
+++ client/templates/tools/randomPage.js
@@ -8,6 +8,7 @@ Template.randomPage.events({
     t="";
     d="";
     i="";
+    id="";
     switch (randomElement) {
       case "evento":
         var e = randomEvents.find({classId: Session.get('classId')}).fetch();
@@ -34,6 +35,10 @@ Template.randomPage.events({
         var r = Math.floor(Math.random() * e.length);
         t = e[r].studentName;
         i= e[r].avatar;
+        id=e[r]._id;
+        if (!i) {
+          i=classes.findOne({_id:Session.get('classId')}).studentImg;
+        }
         if (i.substring(0, 4)!="http") {
           i=images.findOne({_id: i}).image_url;
         }
@@ -43,6 +48,10 @@ Template.randomPage.events({
         var r = Math.floor(Math.random() * e.length);
         t = e[r].groupName;
         i= e[r].groupImg;
+        id=e[r]._id;
+        if (!i) {
+          i=classes.findOne({_id:Session.get('classId')}).groupImg;
+        }
         if (i.substring(0, 4)!="http") {
           i=images.findOne({_id: i}).image_url;
         }
@@ -64,7 +73,38 @@ Template.randomPage.events({
     $("#ModalLabel").text(t);
     $("#ModalDesc").text(d);
     $("#ModalImg").attr("src",i);
+    $("#ModalImg").attr("title",id);
     event.preventDefault();
     Session.setPersistent('navItem',event.target.parentNode.id);
+  },
+  'click #ModalImg': function(event) {
+    event.preventDefault();
+    if ($("#ModalHeader").text()=="estudiante") {
+      Session.setPersistent('studentId', $("#ModalImg").attr("title"));
+      Session.set('studentSelected', true);
+      Session.set('studentSelected', true);
+      Session.setPersistent('sogBtn', "students");
+      Session.set('groupSelected', false);
+      //$(".tab-pane").removeClass("active");
+      $("#studentsMain").addClass("active");
+      $(".nav-pills li").removeClass("active");
+      $("#sM").addClass("active");
+      $("#collapseStudents").removeClass("in");
+      $("#collapseStudents").removeClass("in");
+      $('#randomModal').modal('hide');
+    }
+    if ($("#ModalHeader").text()=="equipo") {
+      Session.setPersistent('groupId', $("#ModalImg").attr("title"));
+      Session.set('groupSelected', true);
+      Session.setPersistent('sogBtn', "groups");
+      Session.set('studentSelected', false);
+      //$(".tab-pane").removeClass("active");
+      $(".nav-pills li").removeClass("active");
+      $("#studentsMain").addClass("active");
+      $("#sM").addClass("active");
+      $("#collapseStudents").removeClass("in");
+      $('#randomModal').modal('hide');
+    }
+    $('#randomModal').modal('hide');
   }
 });
