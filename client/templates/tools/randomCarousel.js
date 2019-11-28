725e89fa7c4d4ef5be3fc1814d2cee155af7f27e
juantoman <juan.torres@iestacio.com>
2019-11-28 14:08:33 +0100

    Carousel per elements

---
+++ client/templates/tools/randomCarousel.js
@@ -0,0 +1,114 @@
+Template.randomCarousel.onRendered(function() {
+
+});
+
+Template.randomCarousel.helpers({
+ students: function() {
+   if ($("#todosR").hasClass("btn-warning")) {
+     return students.find({classId: Session.get('classId')});
+   } else {
+     return students.find({classId: Session.get('classId'),'random':true});
+   }
+ },
+ events: function() {
+   return randomEvents.find({ classId: Session.get('classId') } );
+ },
+ element: function() {
+   return Session.get("randomElement");
+ },
+ image: function(avatar) {
+   avatarVisible=classes.findOne({ _id: Session.get('classId') }).avatarVisible;
+   if ( avatar=="" || !avatar || (  Session.get('userType') != "teacher"  &&  !avatarVisible ) ) {
+     if ( classes.findOne({_id: Session.get('classId')}).studentImg ) {
+       if (classes.findOne({_id: Session.get('classId')}).studentImg.substring(0, 4)=="http") {
+         return classes.findOne({_id: Session.get('classId')}).studentImg;
+       } else {
+         return images.findOne({_id: classes.findOne({_id: Session.get('classId')}).studentImg}).image_url;
+       }
+     } else {
+       return "https://res.cloudinary.com/myclassgame/image/upload/v1542963357/proves/luke.png";
+     }
+   } else  {
+     if (avatar.substring(0, 4)=="http") {
+       return avatar;
+     } else {
+       return images.findOne({_id: avatar}).image_url;
+     }
+   }
+ },
+ equals: function(randomBtn) {
+   if (randomBtn==Session.get("randomElement")) {
+     return true;
+   } else {
+     return false;
+   }
+ }
+});
+
+Template.randomCarousel.events({
+  'click .scene': function(event) {
+    $('.random_carousel').show();
+    var carousel = document.querySelector('.random_carousel');
+    var cells = carousel.querySelectorAll('.carousel__cell');
+    var cellCount; // cellCount set from cells-range input value
+    var selectedIndex = 0;
+    var cellWidth = 210;
+    var cellHeight = 140;
+    var isHorizontal = true;
+    var rotateFn = isHorizontal ? 'rotateY' : 'rotateX';
+    var radius, theta;
+    // console.log( cellWidth, cellHeight );
+
+    function rotateCarousel() {
+      var angle = theta * selectedIndex * -1;
+      carousel.style.transform = 'translateZ(' + -radius + 'px) ' +
+        rotateFn + '(' + angle + 'deg)';
+    }
+
+    function changeCarousel() {
+      //cellCount = cellsRange.value;
+      cellCount = cells.length;
+      theta = 360 / cellCount;
+      var cellSize = isHorizontal ? cellWidth : cellHeight;
+      radius = Math.round( ( cellSize / 2) / Math.tan( Math.PI / cellCount ) );
+      for ( var i=0; i < cells.length; i++ ) {
+        var cell = cells[i];
+        if ( i < cellCount ) {
+          // visible cell
+          cell.style.opacity = 1;
+          var cellAngle = theta * i;
+          cell.style.transform = rotateFn + '(' + cellAngle + 'deg) translateZ(' + radius + 'px)';
+        } else {
+          // hidden cell
+          cell.style.opacity = 0;
+          cell.style.transform = 'none';
+        }
+      }
+
+      //rotateCarousel();
+    }
+    /*
+    var orientationRadios = document.querySelectorAll('input[name="orientation"]');
+    ( function() {
+      for ( var i=0; i < orientationRadios.length; i++ ) {
+        var radio = orientationRadios[i];
+        radio.addEventListener( 'change', onOrientationChange );
+      }
+    })();
+    */
+    function onOrientationChange() {
+      //var checkedRadio = document.querySelector('input[name="orientation"]:checked');
+      //isHorizontal = checkedRadio.value == 'horizontal';
+      rotateFn = isHorizontal ? 'rotateY' : 'rotateX';
+      changeCarousel();
+    }
+
+    // set initials
+    onOrientationChange();
+    var r = Math.floor(Math.random() * cells.length)+cells.length;
+    for (var p=0;p<r;p++){
+      selectedIndex++;
+      rotateCarousel();
+    }
+  }
+});
--- client/templates/tools/randomPage.js
+++ client/templates/tools/randomPage.js
@@ -1,115 +1,14 @@
-Template.randomPage.onRendered(function() {
-  var carousel = document.querySelector('.random_carousel');
-  var cells = carousel.querySelectorAll('.carousel__cell');
-  var cellCount; // cellCount set from cells-range input value
-  var selectedIndex = 0;
-  var cellWidth = 210;
-  var cellHeight = 140;
-  var isHorizontal = true;
-  var rotateFn = isHorizontal ? 'rotateY' : 'rotateX';
-  var radius, theta;
-  // console.log( cellWidth, cellHeight );
-
-  function rotateCarousel() {
-    var angle = theta * selectedIndex * -1;
-    carousel.style.transform = 'translateZ(' + -radius + 'px) ' +
-      rotateFn + '(' + angle + 'deg)';
-  }
-  /*
-  var prevButton = document.querySelector('.previous-button');
-  prevButton.addEventListener( 'click', function() {
-    selectedIndex--;
-    rotateCarousel();
-  });
-  */
-
-  var nextButton = document.querySelector('.next-button');
-  nextButton.addEventListener( 'click', function() {
-    for (var p=0;p<10;p++){
-      selectedIndex++;
-      rotateCarousel();
-    }
-  });
-  /*
-  var cellsRange = document.querySelector('.cells-range');
-  cellsRange.addEventListener( 'change', changeCarousel );
-  cellsRange.addEventListener( 'input', changeCarousel );
-  */
-
-
-  function changeCarousel() {
-    //cellCount = cellsRange.value;
-    cellCount = cells.length
-    theta = 360 / cellCount;
-    var cellSize = isHorizontal ? cellWidth : cellHeight;
-    radius = Math.round( ( cellSize / 2) / Math.tan( Math.PI / cellCount ) );
-    for ( var i=0; i < cells.length; i++ ) {
-      var cell = cells[i];
-      if ( i < cellCount ) {
-        // visible cell
-        cell.style.opacity = 1;
-        var cellAngle = theta * i;
-        cell.style.transform = rotateFn + '(' + cellAngle + 'deg) translateZ(' + radius + 'px)';
-      } else {
-        // hidden cell
-        cell.style.opacity = 0;
-        cell.style.transform = 'none';
-      }
-    }
-
-    rotateCarousel();
-  }
-  /*
-  var orientationRadios = document.querySelectorAll('input[name="orientation"]');
-  ( function() {
-    for ( var i=0; i < orientationRadios.length; i++ ) {
-      var radio = orientationRadios[i];
-      radio.addEventListener( 'change', onOrientationChange );
-    }
-  })();
-  */
-  function onOrientationChange() {
-    //var checkedRadio = document.querySelector('input[name="orientation"]:checked');
-    //isHorizontal = checkedRadio.value == 'horizontal';
-    rotateFn = isHorizontal ? 'rotateY' : 'rotateX';
-    changeCarousel();
-  }
-
-  // set initials
-  onOrientationChange();
-});
-
-Template.randomPage.helpers({
- students: function() {
-   return students.find({ classId: Session.get('classId') } );
- },
- image: function(avatar) {
-   avatarVisible=classes.findOne({ _id: Session.get('classId') }).avatarVisible;
-   if ( avatar=="" || !avatar || (  Session.get('userType') != "teacher"  &&  !avatarVisible ) ) {
-     if ( classes.findOne({_id: Session.get('classId')}).studentImg ) {
-       if (classes.findOne({_id: Session.get('classId')}).studentImg.substring(0, 4)=="http") {
-         return classes.findOne({_id: Session.get('classId')}).studentImg;
-       } else {
-         return images.findOne({_id: classes.findOne({_id: Session.get('classId')}).studentImg}).image_url;
-       }
-     } else {
-       return "https://res.cloudinary.com/myclassgame/image/upload/v1542963357/proves/luke.png";
-     }
-   } else  {
-     if (avatar.substring(0, 4)=="http") {
-       return avatar;
-     } else {
-       return images.findOne({_id: avatar}).image_url;
-     }
-   }
- },
-});
-
 Template.randomPage.events({
   'click a': function(event) {
     randomElement=event.target.id;
     Session.set("randomElement",randomElement);
-    Modal.show("randomModal");
+    $('.random_carousel').hide();
+    switch (randomElement) {
+      case "todosR":
+        $(event.target).toggleClass("btn-warning");
+        break;
+    }
+    //Modal.show("randomModal");
     /*
     $("#ModalHeader").text("RANDOM");
     $("#ModalLabel").text("");
