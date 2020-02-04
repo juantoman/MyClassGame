041ba5eda27c2c3680efa6cc331d829a7464be20
juantoman <juan.torres@iestacio.com>
2020-02-04 13:44:18 +0100

    1.2.9 Funcionalitat botons batalles SxM SxS

--- client/templates/batle/battle.js
+++ client/templates/batle/battle.js
@@ -1,29 +1,35 @@
-Template.battle.onRendered(function() {
-  $('#sn').html(classes.findOne({ _id: Session.get('classId') } ).adventureDesc);
-});
+var studentId1Battle='X6vu8aDFrGYinoXEd';
+
 Template.battle.helpers({
-  class: function() {
-    return classes.findOne({ _id: Session.get('classId') } );
+  student1Battle: function() {
+    return students.findOne({'_id': Session.get('studentId1Battle')});
   },
-  studentName: function() {
-    return students.findOne({_id: Session.get('currentStudent')}).studentName;
+  student2Battle: function() {
+    return students.findOne({'_id': Session.get('studentId2Battle')});
   },
-  mission: function() {
-    t=students.findOne({_id: Session.get('currentStudent')}).activeTask;
-    m=chalMissions.findOne({_id: t}).missionId;
-    return challenges.findOne({_id: m});
-  },
-  task: function() {
-    t=students.findOne({_id: Session.get('currentStudent')}).activeTask;
-    return chalMissions.findOne({_id: t});
+  image: function(avatar) {
+    avatarVisible=classes.findOne({ _id: Session.get('classId') }).avatarVisible;
+    if ( avatar=="" || !avatar || (  Session.get('userType') != "teacher"  &&  !avatarVisible ) ) {
+      if ( classes.findOne({_id: Session.get('classId')}).studentImg ) {
+        if (classes.findOne({_id: Session.get('classId')}).studentImg.substring(0, 4)=="http") {
+          return classes.findOne({_id: Session.get('classId')}).studentImg;
+        } else {
+          return images.findOne({_id: classes.findOne({_id: Session.get('classId')}).studentImg}).image_url;
+        }
+      } else {
+        return "https://res.cloudinary.com/myclassgame/image/upload/v1542963357/proves/luke.png";
+      }
+    } else  {
+      if (avatar.substring(0, 4)=="http") {
+        return avatar;
+      } else {
+        return images.findOne({_id: avatar}).image_url;
+      }
+    }
   }
 });
 
 Template.battle.events({
-  'click #student_th': function(event) {
-    event.preventDefault();
-    sort.set({ student: 1 });
-  },
   'click .battleHeart': function(event) {
     event.preventDefault();
     //$(event.currentTarget).toggleClass('oculto');
@@ -61,9 +67,32 @@ Template.battle.events({
       });
     }
   },
-  'click .battle .card,.battle .card-turned': function(event) {
+  /*'click .battle .card,.battle .card-turned': function(event) {*/
+  'click .battle .card': function(event) {
     $(event.currentTarget).toggleClass('card card-turned');
     $(event.currentTarget).find('.card-inside').toggleClass('card-back card-front');
     $(event.currentTarget).find('.backlogocard').toggleClass('oculto');
+  },
+  'click #svsmBattle': function(event) {
+    if ( students.find({'classId': Session.get('classId'),'random': true }).count() == 1 ) {
+      Meteor.call('allRandomStudents',Session.get('classId'));
+    }
+    var e = students.find({classId: Session.get('classId'),'random':true}).fetch();
+    var r = Math.floor(Math.random() * e.length);
+    Session.set('studentId1Battle',e[r]._id);
+    $('#card1Battle').click();
+  },
+  'click #svssBattle': function(event) {
+    if ( students.find({'classId': Session.get('classId'),'random': true }).count() == 1 ) {
+      Meteor.call('allRandomStudents',Session.get('classId'));
+    }
+    var e1 = students.find({classId: Session.get('classId'),'random':true}).fetch();
+    var r1 = Math.floor(Math.random() * e1.length);
+    var e2 = students.find({classId: Session.get('classId'),'random':true}).fetch();
+    var r2 = Math.floor(Math.random() * e2.length);
+    Session.set('studentId1Battle',e1[r1]._id);
+    Session.set('studentId2Battle',e2[r2]._id);
+    $('#card1Battle').click();
+    $('#card2Battle').click();
   }
 });
