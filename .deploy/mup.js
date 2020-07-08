11525021c51563bfc8d053198915e802ab0f6790
juantoman <juan.torres@iestacio.com>
2020-07-08 13:59:43 +0200

    Errada

--- .deploy/mup.js
+++ .deploy/mup.js
@@ -5,7 +5,7 @@ module.exports = {
       host: '51.68.126.115',
       username: 'root',
       // pem: './path/to/pem'
-      password: 'Mcg20holita06?'
+      password: ''
       // or neither for authenticate from ssh-agent
     }
   },
