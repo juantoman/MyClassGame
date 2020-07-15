7da0b5a6fc90d2c076cd56892215be7f25089763
juantoman <juan.torres@iestacio.com>
2020-07-15 13:11:28 +0200

    Peu reduit i enllaços externs per Android

--- mobile-config.js
+++ mobile-config.js
@@ -5,7 +5,7 @@ App.info({
   author: '@juantoman',
   email: 'myclassgame@gmail.com',
   website: 'https://www.myclassgame.tk',
-  version: "2.0.1"
+  version: "2.0.3"
 });
 
 // Set up resources such as icons and launch screens.
