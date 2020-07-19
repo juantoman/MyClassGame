3818b74ed1711305ee7a09cfcd5b5670e9695982
juantoman <juan.torres@iestacio.com>
2020-07-19 11:08:38 +0200

    Millorar agregar docents

--- client/templates/settingsPage/teachers/teachers.js
+++ client/templates/settingsPage/teachers/teachers.js
@@ -14,21 +14,19 @@ Template.teachersTemplate.helpers({
       return false;
     }
   },
-  email: function() {
-    try {
-      emailUser=Meteor.users.findOne({_id: this.teacherId}).emails[0].address;
+  googleOAuth: function(teacherId) {
+    if ( Meteor.users.find( { '_id': this._id, 'services.google.email' : { $exists: true } } ).count() > 0 ) {
+      return true;
+    } else {
+      return false;
     }
-    catch(err) {
-      emailUser=Meteor.users.findOne({_id: this.teacherId}).services.google.email;
-    }
-    return emailUser;
   },
-  teacherEmail: function() {
+  email: function(teacherId) {
     try {
-      emailUser=Meteor.users.findOne({_id: this._id}).emails[0].address;
+      emailUser=Meteor.users.findOne({_id: teacherId}).emails[0].address;
     }
     catch(err) {
-      emailUser=Meteor.users.findOne({_id: this._id}).services.google.email;
+      emailUser=Meteor.users.findOne({_id: teacherId}).services.google.email;
     }
     return emailUser;
   }
@@ -38,14 +36,47 @@ Template.teachersTemplate.events({
   'submit form#newTeachers': function(event) {
     event.preventDefault();
     teacherEmail=$(event.target).find('[name=teacherEmail]').val();
-    try {
+    if ( Meteor.users.find( { 'emails.address' : teacherEmail } ).count() > 0 ) {
       teacherId=Meteor.users.findOne({'emails.address':teacherEmail})._id;
+      if ( Meteor.users.find( { '_id' : teacherId, 'classesTeacher' : Session.get('classId') } ).count() == 0 ) {
+        Meteor.call('teacherAccepted',Session.get("classId"),teacherId);
+        swal({
+          title: TAPi18n.__('teacherAccepted'),
+          type: 'success'
+        })
+      } else {
+        swal({
+          title: TAPi18n.__('teacherInClassroom'),
+          type: 'warning'
+        })
+      }
+    } else {
+      swal({
+        title: TAPi18n.__('teacherNotFound'),
+        type: 'warning'
+      })
     }
-    catch(err) {
+    if ( Meteor.users.find( { 'services.google.email' : teacherEmail } ).count() > 0 ) {
       teacherId=Meteor.users.findOne({'services.google.email':teacherEmail})._id;
+      if ( Meteor.users.find( { '_id' : teacherId, 'classesTeacher' : Session.get('classId') } ).count() == 0 ) {
+        Meteor.call('teacherAccepted',Session.get("classId"),teacherId);
+        swal({
+          title: TAPi18n.__('teacherAccepted'),
+          type: 'success'
+        })
+      } else {
+        swal({
+          title: TAPi18n.__('teacherInClassroom'),
+          type: 'warning'
+        })
+      }
+    } else {
+      swal({
+        title: TAPi18n.__('teacherNotFound'),
+        type: 'warning'
+      })
     }
     //Meteor.call('addTeacherInClass',Session.get("classId"),teacherId);
-    Meteor.call('teacherAccepted',Session.get("classId"),teacherId);
   },
   'click .acceptTeacher': function(event) {
     event.preventDefault();
@@ -57,9 +88,27 @@ Template.teachersTemplate.events({
     event.preventDefault();
     n=Meteor.users.find({'classesTeacher': Session.get('classId')}).count();
     if (n>1) {
-      Meteor.call('teacherRemoved',Session.get("classId"),this._id);
+      swal({
+        title: TAPi18n.__('delete') + " " + TAPi18n.__('teacher'),
+        text: TAPi18n.__('areYouSure'),
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonText: TAPi18n.__('yes'),
+        cancelButtonText: 'No'
+      }).then((result) => {
+        if (result.value) {
+          Meteor.call('teacherRemoved',Session.get("classId"),this._id);
+          swal({
+            title: TAPi18n.__('teacher') + " " + TAPi18n.__('deleted'),
+            type: 'success'
+          })
+        }
+      })
     } else {
-      alert("No puede quedarse sin profes")
+      swal({
+        title: TAPi18n.__('noteacher'),
+        type: 'warning'
+      })
     }
   }
 });
