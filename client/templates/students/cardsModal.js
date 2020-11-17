caf6ce565d0573fd546358bf35764dccee01f12b
juantoman <juan.torres@iestacio.com>
2020-11-17 12:14:10 +0100

    Actualitzar col·leccions alumnes 7

--- client/templates/students/cardsModal.js
+++ client/templates/students/cardsModal.js
@@ -45,15 +45,57 @@ Template.cardsModal.events({
         Meteor.call('studentCardPull', Session.get('studentId'), cardId);
       }
     });
-    $('.chromesModal').find(".list-group-item").each( function() {
-      chromeId=this.id;
-      if ($(this).hasClass("list-group-item-danger")) {
-        if (students.find({'_id':Session.get('studentId'),'chromes.chromeId':chromeId}).count() != 1 ) {
-          Meteor.call('studentChrome', Session.get('studentId'), chromeId);
-        }
+    l=0;
+    s=students.findOne({_id: Session.get('studentId')});
+    xpChecked=classes.findOne({_id: Session.get('classId')}).xpChangeLevel;
+    if (xpChecked) {
+      levelXP=classes.findOne({_id: Session.get('classId')}).levelXP;
+      l=parseInt(s.XP/levelXP);
+    } else {
+      l=parseInt(s.level);
+    }
+    $('.chromesModal').find(".list-group-item-danger").each( function(i) {
+      chromeId=this.getAttribute('data-chromeId');
+      c=chromes.findOne({_id: chromeId})
+      if (c.chromeLevel > l ) {
+        swal({
+          title: TAPi18n.__('lowLevel'),
+          type: 'warning'
+        })
+      } else if ( c.chromePrice > s.coins ) {
+        swal({
+          title: TAPi18n.__('noMoney'),
+          type: 'warning'
+        })
       } else {
-        Meteor.call('studentChromePull', Session.get('studentId'), chromeId);
+        swal({
+          title: TAPi18n.__('add') + " " +  TAPi18n.__('collectionable'),
+          text: TAPi18n.__('areYouSure'),
+          type: 'warning',
+          showCancelButton: true,
+          confirmButtonText: TAPi18n.__('yes'),
+          cancelButtonText: 'No'
+        }).then((result) => {
+          if (result.value) {
+              Meteor.call('studentChrome', Session.get('studentId'), chromeId);
+              Meteor.call('incCoins', Session.get('studentId'), -parseInt(c.chromePrice));
+              // Meteor.call('studentBadge', Session.get('studentId'), this._id);
+              // Meteor.call('studentXP', Session.get('studentId'), parseInt(this.points));
+            swal({
+              title: TAPi18n.__('collectionable') + " " +  TAPi18n.__('added'),
+              type: 'success'
+            })
+          // result.dismiss can be 'overlay',e 'cancel', 'close', 'esc', 'timer'
+          }
+        })
       }
+      //Meteor.call('studentChrome', Session.get('studentId'), chromeId);
+      //   if (students.find({'_id':Session.get('studentId'),'chromes.chromeId':chromeId}).count() != 1 ) {
+      //     Meteor.call('studentChrome', Session.get('studentId'), chromeId);
+      //   }
+      // } else {
+      //   Meteor.call('studentChromePull', Session.get('studentId'), chromeId);
+      // }
     });
     Modal.hide('cardsModal');
   },
