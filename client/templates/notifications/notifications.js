99b9253c32b1c0d7af7341a687e6356f68c3f2be
juantmn <juan.torres@iestacio.com>
2020-11-28 09:38:48 +0100

    Noves Notificacions

--- client/templates/notifications/notifications.js
+++ client/templates/notifications/notifications.js
@@ -34,29 +34,54 @@ Template.notifications.helpers({
 Template.notifications.events({
  'click .validarCard': function(event) {
     event.preventDefault();
-    swal({
-      title: TAPi18n.__('use') + " " + TAPi18n.__('power'),
-      text: TAPi18n.__('areYouSure'),
-      type: 'warning',
-      showCancelButton: true,
-      confirmButtonText: TAPi18n.__('yes'),
-      cancelButtonText: 'No'
-    }).then((result) => {
-      if (result.value) {
-        Meteor.call('studentUseCard', this.studentId, this.elementId);
-        Meteor.call('usedCard', this._id);
-        swal({
-          title: TAPi18n.__('canUse') + " " + TAPi18n.__('power'),
-          type: 'success'
-        })
-      // result.dismiss can be 'overlay', 'cancel', 'close', 'esc', 'timer'
-      }
-    })
+    if ( !this.used ) {
+      swal({
+        title: TAPi18n.__('use') + " " + TAPi18n.__('power'),
+        text: TAPi18n.__('areYouSure'),
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonText: TAPi18n.__('yes'),
+        cancelButtonText: 'No'
+      }).then((result) => {
+        if (result.value) {
+          Meteor.call('studentUseCard', this.studentId, this.elementId);
+          Meteor.call('usedCard', this._id);
+          swal({
+            title: TAPi18n.__('canUse') + " " + TAPi18n.__('power'),
+            type: 'success'
+          })
+        // result.dismiss can be 'overlay', 'cancel', 'close', 'esc', 'timer'
+        }
+      })
+    }
   },
   'click .validarItem': function(event) {
     event.preventDefault();
+    if ( !this.used ) {
+      swal({
+        title: TAPi18n.__('use') + " " + TAPi18n.__('item'),
+        text: TAPi18n.__('areYouSure'),
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonText: TAPi18n.__('yes'),
+        cancelButtonText: 'No'
+      }).then((result) => {
+        if (result.value) {
+          Meteor.call('studentUseItem', this.studentId, this.elementId);
+          Meteor.call('usedItem', this._id);
+          swal({
+            title: TAPi18n.__('canUse') + " " + TAPi18n.__('item'),
+            type: 'success'
+          })
+        // result.dismiss can be 'overlay',e 'cancel', 'close', 'esc', 'timer'
+        }
+      })
+    }
+  },
+  'click .notification-remove': function(event) {
+    event.preventDefault();
     swal({
-      title: TAPi18n.__('use') + " " + TAPi18n.__('item'),
+      title: TAPi18n.__('remove') + " " + TAPi18n.__('notification'),
       text: TAPi18n.__('areYouSure'),
       type: 'warning',
       showCancelButton: true,
@@ -64,12 +89,7 @@ Template.notifications.events({
       cancelButtonText: 'No'
     }).then((result) => {
       if (result.value) {
-        Meteor.call('studentUseItem', this.studentId, this.elementId);
-        Meteor.call('usedItem', this._id);
-        swal({
-          title: TAPi18n.__('canUse') + " " + TAPi18n.__('item'),
-          type: 'success'
-        })
+        Meteor.call('notificationRemove', this._id);
       // result.dismiss can be 'overlay',e 'cancel', 'close', 'esc', 'timer'
       }
     })
