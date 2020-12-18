2da78844a18de1d58370424c1d55c36edb49d248
juantoman <juan.torres@iestacio.com>
2020-12-18 11:15:59 +0100

    Millorar qüestionaris 17

--- client/templates/questions/questionModal.js
+++ client/templates/questions/questionModal.js
@@ -1,7 +1,19 @@
 Template.questionModal.helpers({
   questionSelected: function() {
     return questions.findOne({'_id': Session.get('questionId')});
-  }
+  },
+  questionImageUrl: function() {
+    img=this.questionImage;
+    //return "https://res.cloudinary.com/myclassgame/image/upload/q_auto,w_auto,h_180,f_auto,dpr_auto/v1582290869/myclassgame/darth-vader-pajamas-officially-licensed-merch-the-23_kjngzn.png";
+    if (img.substring(0, 4)=="http") {
+      img=img.replace('/upload/','/upload/q_auto,w_auto,h_180,f_auto,dpr_auto/');
+      return img;
+    } else {
+      cloudinary_url=images.findOne({_id: img}).image_url;
+      cloudinary_url=cloudinary_url.replace('/upload/','/upload/q_auto,w_auto,h_180,f_auto,dpr_auto/');
+      return cloudinary_url;
+    }
+  },
 })
 
 Template.questionModal.events({
