96d5b44218b17afd94f88db9d712af38df1b1c28
myclassgame <juan.torres@iestacio.com>
2021-03-29 11:16:18 +0200

    Nous poders 3

---
+++ client/templates/gamification/myComponents/cardsList/powercard.js
@@ -0,0 +1,26 @@
+Template.powercard.helpers({
+  card_src: function(imageId) {
+    if (imageId) {
+      cloudinary_url=images.findOne({_id: imageId}).image_url;
+      cloudinary_url=cloudinary_url.replace('/upload/','/upload/q_auto,w_auto,h_100,f_auto,dpr_auto/');
+      return cloudinary_url;
+    } else {
+      if (Session.get('selectedImage')) {
+        cloudinary_url=images.findOne({_id: Session.get('selectedImage')}).image_url;
+        cloudinary_url=cloudinary_url.replace('/upload/','/upload/q_auto,w_auto,h_100,f_auto,dpr_auto/');
+        return cloudinary_url;
+      } else {
+        return "https://res.cloudinary.com/myclassgame/image/upload/q_auto,w_auto,h_100,f_auto,dpr_auto/v1554810211/images/event-2930674_960_720.png";
+      }
+    }
+  }
+});
+
+Template.powercard.events({
+ 'click img, click .glyphicon': function(event) {
+    event.preventDefault();
+    Session.set('imageType','card');
+    Session.set('idElementImage',this._id);
+    Modal.show('imagesTemplate');
+  }
+});
