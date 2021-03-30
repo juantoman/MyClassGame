3d7e09dd693c1ff45e102a38293cdc4e6ff61ec5
juantoman <juan.torres@iestacio.com>
2021-03-30 09:56:16 +0200

    Nous poders 4

--- client/templates/gamification/myComponents/cardsList/powercard.js
+++ client/templates/gamification/myComponents/cardsList/powercard.js
@@ -2,15 +2,15 @@ Template.powercard.helpers({
   card_src: function(imageId) {
     if (imageId) {
       cloudinary_url=images.findOne({_id: imageId}).image_url;
-      cloudinary_url=cloudinary_url.replace('/upload/','/upload/q_auto,w_auto,h_100,f_auto,dpr_auto/');
+      cloudinary_url=cloudinary_url.replace('/upload/','/upload/q_auto,w_auto,h_200,f_auto,dpr_auto/');
       return cloudinary_url;
     } else {
       if (Session.get('selectedImage')) {
         cloudinary_url=images.findOne({_id: Session.get('selectedImage')}).image_url;
-        cloudinary_url=cloudinary_url.replace('/upload/','/upload/q_auto,w_auto,h_100,f_auto,dpr_auto/');
+        cloudinary_url=cloudinary_url.replace('/upload/','/upload/q_auto,w_auto,h_200,f_auto,dpr_auto/');
         return cloudinary_url;
       } else {
-        return "https://res.cloudinary.com/myclassgame/image/upload/q_auto,w_auto,h_100,f_auto,dpr_auto/v1554810211/images/event-2930674_960_720.png";
+        return "https://res.cloudinary.com/myclassgame/image/upload/q_auto,w_auto,h_200,f_auto,dpr_auto/v1554810211/images/event-2930674_960_720.png";
       }
     }
   }
