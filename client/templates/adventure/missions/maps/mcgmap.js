0b6a9ce4f35ea084e5f87926de973e2b7e80fb13
juantmn <juan.torres@iestacio.com>
2021-05-19 00:08:26 +0200

    Proves vis.js 5

--- client/templates/adventure/missions/maps/mcgmap.js
+++ client/templates/adventure/missions/maps/mcgmap.js
@@ -9,17 +9,29 @@ Template.mcgmap.events({
   'click #verMapa': function(event) {
     event.preventDefault();
     $('#missionMap').toggleClass("oculto");
+    if(!$('#missionMap').hasClass("oculto")) {
     // create an array with nodes
       m=challenges.find({classId: Session.get('classId')}, {sort: {order: 1}}).fetch();
       n=m.length;
       var nodes = new vis.DataSet();
       var edges = new vis.DataSet();
       for (i=1;i<=n;i++) {
+        if (m[i-1].x) {
+          x=parseInt(m[i-1].x);
+        } else {
+          x=100*i;
+        }
+        if (m[i-1].y) {
+          y=parseInt(m[i-1].y);
+        } else {
+          y=100;
+        }
+        console.log(x +" "+ y)
         node={
             id: i,
             label: "M"+i,
-            x:100*i,
-            y:100,
+            x:x,
+            y:y,
             color: "white"
         };
         nodes.add(node);
@@ -49,7 +61,8 @@ Template.mcgmap.events({
               smooth: false
         },
         interaction:{
-              zoomView: false
+              zoomView: false,
+              dragView: false
         }
       };
 
@@ -57,5 +70,20 @@ Template.mcgmap.events({
 
       // initialize your network!
       var network = new vis.Network(container, data, options);
+
+      network.on("dragEnd", function (params) {
+    	  params.event = "[original event]";
+    	  document.getElementById("eventSpanHeading").innerText = "dragEnd event:";
+    	  document.getElementById("eventSpanContent").innerText = JSON.stringify(params,null,4);
+    	  coord={
+          x:parseInt(params.pointer.canvas.x),
+          y:parseInt(params.pointer.canvas.y-400)
+        }
+        console.log(parseInt(params.pointer.DOM.x) +" "+ parseInt(params.pointer.DOM.y));
+        mId=challenges.findOne({classId: Session.get('classId'), order: params.nodes[0]})._id;
+        Meteor.call('chalUpdate', mId, coord);
+    	  console.log("dragEnd event, getNodeAt returns: " + this.getNodeAt(params.pointer.DOM));
+      });
+    }
   }
 });
