249f0f738fe9b2502e356600285bd03c86c6596c
juantmn <juan.torres@iestacio.com>
2021-05-24 07:09:27 +0200

    Proves vis.js 9

--- client/templates/adventure/missions/maps/mcgmap.js
+++ client/templates/adventure/missions/maps/mcgmap.js
@@ -1,78 +1,14 @@
 $.getScript("https://unpkg.com/vis-network/standalone/umd/vis-network.min.js");
 
 Template.mcgmap.onRendered(function() {
-  const nodes = [
-    { id: 1, label: 1 },
-    { id: 2, label: 2 },
-    { id: 3, label: 3 },
-    { id: 4, label: 4 },
-  ]
-
-  const edges = [
-    { id: '1-2',  from: 1, to: 2 },
-    { id: '1-3', from: 1, to: 3 },
-    { id: '2-3', from: 2, to: 3 },
-    { id: '1-4', from: 1, to: 4 },
-  ]
-
-  const positionsElement = document.getElementById('positions')
-  const container = document.getElementById('missionMap')
-  const data = {
-    nodes: new vis.DataSet(nodes),
-    edges: new vis.DataSet(edges),
-  }
-  const options = {
-    width: '1000px',
-    height: '800px',
-    layout: {
-      improvedLayout: false,
-    },
-    edges: {
-      smooth: false,
-    },
-    physics: false,
-    interaction: {
-        dragNodes: true,// do not allow dragging nodes
-        zoomView: false, // do not allow zooming
-        dragView: false  // do not allow dragging
-    }
-  }
-
-  let network = null
-
-  function initGraph(generateRandomPosition = true) {
-    if (generateRandomPosition) {
-      data.nodes.forEach(node => {
-        data.nodes.update({ id: node.id, x: undefined, x: undefined })
-      })
-    }
-    network = new vis.Network(container, data, options);
-    // network.moveTo({
-    //     position: {x: 0, y: 0}
-    // })
-  }
-
-  document.getElementById('generate-graph').addEventListener('click', initGraph)
-
-  document.getElementById('extract-positions').addEventListener('click', e => {
-    network.storePositions()
-    const nodePositions = data.nodes.map(({ id, x, y }) => ({ id, x, y }))
-    positionsElement.value = JSON.stringify(nodePositions)
-  })
-
-  document.getElementById('load-positions').addEventListener('click', e => {
-    const nodePositions = JSON.parse(positionsElement.value)
-    nodePositions.forEach(nodePosition => data.nodes.update(nodePosition))
-    initGraph(false)
-  })
 
 })
 
 Template.mcgmap.events({
   'click #verMapa': function(event) {
     event.preventDefault();
-    $('#missionMap').toggleClass("oculto");
-    if(!$('#missionMap').hasClass("oculto")) {
+    $('#missionMapContainer').toggleClass("oculto");
+    if(!$('#missionMapContainer').hasClass("oculto")) {
     // create an array with nodes
       m=challenges.find({classId: Session.get('classId')}, {sort: {order: 1}}).fetch();
       n=m.length;
@@ -113,6 +49,13 @@ Template.mcgmap.events({
       // create a network
       var container = document.getElementById('missionMap');
 
+      var img = document.getElementById('missionMapImage');
+      var width = img.clientWidth;
+      var height = img.clientHeight;
+      var widthIni=1697;
+      var r=width/widthIni;
+      alert(width)
+
       // provide the data in the vis format
       var data = {
           nodes: nodes,
@@ -120,8 +63,8 @@ Template.mcgmap.events({
       };
       var options = {
         autoResize: false,
-        width: '1000px',
-        height: '800px',
+        width: parseInt(width/r) + 'px',
+        height: parseInt(height/r) +'px',
         physics:{
               enabled: false
         },
@@ -148,20 +91,27 @@ Template.mcgmap.events({
           // offset: {x: -width/2, y: -height/2},
           // scale: 1
       })
+      network.setSize(width,height);
+      network.redraw();
 
       network.on("dragEnd", function (params) {
         //https://stackoverflow.com/questions/40489700/visjs-save-manipulated-data-to-json
-    	  params.event = "[original event]";
-    	  document.getElementById("eventSpanHeading").innerText = "dragEnd event:";
-    	  document.getElementById("eventSpanContent").innerText = JSON.stringify(params,null,4);
     	  coord={
           x:parseInt(params.pointer.canvas.x),
           y:parseInt(params.pointer.canvas.y)
         }
         mId=challenges.findOne({classId: Session.get('classId'), order: params.nodes[0]})._id;
         Meteor.call('chalUpdate', mId, coord);
-    	  console.log("dragEnd event, getNodeAt returns: " + this.getNodeAt(params.pointer.DOM));
       });
+
+      window.visualViewport.addEventListener("resize", viewportHandler);
+      function viewportHandler(event) {
+        // NOTE: This doesn't actually work at time of writing
+        var width = img.clientWidth;
+        var height = img.clientHeight;
+        network.setSize(width,height);
+        network.redraw();
+      }
     }
   }
 });
