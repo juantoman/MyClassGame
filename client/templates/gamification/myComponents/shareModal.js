00e068805d42411709e2da4ace0f77f52b3caf10
juantoman <juan.torres@iestacio.com>
2022-06-28 12:58:48 +0200

    Compartir Coleccionables + QR

--- client/templates/gamification/myComponents/shareModal.js
+++ client/templates/gamification/myComponents/shareModal.js
@@ -1,3 +1,6 @@
+
+$.getScript("https://cdn.rawgit.com/davidshimjs/qrcodejs/gh-pages/qrcode.min.js");
+
 Template.shareModal.events({
     'click .shareModalClose': function(event) {
         event.preventDefault();
@@ -13,7 +16,13 @@ Template.shareModal.events({
         event.preventDefault();
         var sharedElementId=Session.get("sharedElementId");
         var iframe = '<div style="width: 100%;"><div style="position: relative; padding-bottom: 56.25%; padding-top: 0; height: 0;"><iframe frameborder="0" width="1200" height="675" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;" src="https://www.myclassgame.es/mcgapi/collectionableAPI.html?e=collectionable&amp;id='+ sharedElementId +'" type="text/html" allowfullscreen="true" scrolling="yes"></iframe></div></div>'
-        alert(iframe)
         navigator.clipboard.writeText(iframe)
+    },
+    'click .shareModalQRCode': function(event) {
+        event.preventDefault();
+        var sharedElementId=Session.get("sharedElementId");
+        var url = "https://www.myclassgame.es/mcgapi/collectionableAPI.html?e=collectionable&amp;id="+ sharedElementId;
+        new QRCode(document.getElementById("qrcode"), url);
+        document.getElementById("btnQrCode").disabled = true;
     }
 });
