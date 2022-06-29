811b9438bdca615ba537ab72e2c7354a742ada77
juantoman <juan.torres@iestacio.com>
2022-06-29 10:18:56 +0200

    Correcció QR Code

--- client/templates/gamification/myComponents/shareModal.js
+++ client/templates/gamification/myComponents/shareModal.js
@@ -7,7 +7,7 @@ Template.shareModal.onRendered(function() {
  Template.shareModal.helpers({
     url: function() {
         var sharedElement=Session.get("sharedElement");
-        var url = "https://www.myclassgame.es/mcgapi/"+sharedElement.type+"API.html?e="+sharedElement.type+"&amp;id="+ sharedElement.id;
+        var url = "https://www.myclassgame.es/mcgapi/"+sharedElement.type+"API.html?e="+sharedElement.type+"&id="+ sharedElement.id;
         return url;
     }
   });
@@ -20,20 +20,20 @@ Template.shareModal.events({
     'click .shareModalURL': function(event) {
         event.preventDefault();
         var sharedElement=Session.get("sharedElement");
-        var url = "https://www.myclassgame.es/mcgapi/"+sharedElement.type+"API.html?e="+sharedElement.type+"&amp;id="+ sharedElement.id;
+        var url = "https://www.myclassgame.es/mcgapi/"+sharedElement.type+"API.html?e="+sharedElement.type+"&id="+ sharedElement.id;
         navigator.clipboard.writeText(url)
     },
     'click .shareModalIframe': function(event) {
         event.preventDefault();
         var sharedElement=Session.get("sharedElement");
-        var url = "https://www.myclassgame.es/mcgapi/"+sharedElement.type+"API.html?e="+sharedElement.type+"&amp;id="+ sharedElement.id;
+        var url = "https://www.myclassgame.es/mcgapi/"+sharedElement.type+"API.html?e="+sharedElement.type+"&id="+ sharedElement.id;
         var iframe = '<div style="width: 100%;"><div style="position: relative; padding-bottom: 56.25%; padding-top: 0; height: 0;"><iframe frameborder="0" width="1200" height="675" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;" src="'+ url +'" type="text/html" allowfullscreen="true" scrolling="yes"></iframe></div></div>'
         navigator.clipboard.writeText(iframe)
     },
     'click .shareModalQRCode': function(event) {
         event.preventDefault();
         var sharedElement=Session.get("sharedElement");
-        var url = "https://www.myclassgame.es/mcgapi/"+sharedElement.type+"API.html?e="+sharedElement.type+"&amp;id="+ sharedElement.id;
+        var url = "https://www.myclassgame.es/mcgapi/"+sharedElement.type+"API.html?e="+sharedElement.type+"&id="+ sharedElement.id;
         qrCode = new QRCodeStyling({
             width: 200,
             height: 200,
